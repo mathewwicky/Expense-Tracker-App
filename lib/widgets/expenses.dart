ed0eee3c3b0ffb17a19b2129e574fc4a8c23bc0f
mathew <mathewwicky8@gmail.com>
2024-11-28 03:58:56 +0200

    created card for the items expense details

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,55 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 19.9,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinematic ',
+      amount: 20.9,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'cus ticket',
+      amount: 29.9,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'chicken packs',
+      amount: 11.9,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('the chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
