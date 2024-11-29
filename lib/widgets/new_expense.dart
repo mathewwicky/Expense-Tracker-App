c77b2fc62ed04ce6ab69b4a9919016129e24d2a9
mathew <mathewwicky8@gmail.com>
2024-11-29 03:27:39 +0200

    introduced TextEditing Controller to help in saving user input values

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,6 +8,22 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = '';
+
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+
+  final _titleController = TextEditingController();
+  final _amoutController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amoutController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,11 +31,43 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
+            //onChanged: _saveTitleInput,
+
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amoutController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amout'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // print(_enteredTitle);
+                  print(_titleController.text);
+                  print(_amoutController.text);
+                },
+                child: Text('save expense'),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
+            ],
+          )
         ],
       ),
     );
